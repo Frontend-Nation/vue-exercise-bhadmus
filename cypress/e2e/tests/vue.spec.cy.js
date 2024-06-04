3db0593aed040ccea663272495b90dfa7484e8b2
Ademola Bhadmus <sanguine@Ademolas-MBP.lan>
2024-06-04 23:36:30 +0100

    renamed file

---
+++ cypress/e2e/tests/vue.spec.cy.js
@@ -0,0 +1,17 @@
+let data
+describe('Test Login Scenario', () => {
+  before(()=>{
+    cy.fixture('element-mapper').then(sel=>{
+      data = sel
+    })
+  })
+
+  it('Use the search feature', ()=>{
+  })
+
+  it('Pick Quick Start from the Doc list and handle a tab redirect', ()=>{
+  })
+
+  it('Pick Quick Start from the Doc list', () => {
+  })
+})
