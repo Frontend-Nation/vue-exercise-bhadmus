ccdaae4279abbf3c290dfdb155e0149cf86e9589
Ademola Bhadmus <sanguine@Ademolas-MBP.lan>
2024-06-04 23:32:21 +0100

    boiler plate

--- cypress/e2e/tests/orange.spec.cy.js
+++ cypress/e2e/tests/orange.spec.cy.js
@@ -7,22 +7,11 @@ describe('Test Login Scenario', () => {
   })
 
   it('Use the search feature', ()=>{
-    cy.get(data.searchButton).should('be.visible').click()
-    cy.get(data.searchField).should('exist').type('Dynamic Naming{enter}')
-    cy.get(data.searchedHeader).should('be.visible').and('contain.text', 'Dynamic Argument Syntax Constraints')
   })
 
   it('Pick Quick Start from the Doc list and handle a tab redirect', ()=>{
-    cy.get(data.docTab).should('be.visible').trigger('mouseenter')
-    cy.get(data.quickStartOption).contains('Quick Start').should('be.visible').click()
-    cy.get(data.quickStartHeader).should('be.visible').and('contain.text', 'Quick Start')
-    cy.get(data.playgroundLink).invoke('attr','target','_self').click()
-    cy.get(data.playgroundHeader).should('be.visible').and('contain.text', 'Vue SFC Playground')
   })
 
   it('Pick Quick Start from the Doc list', () => {
-    cy.get(data.docTab).should('be.visible').trigger('mouseenter')
-    cy.get(data.exampleOption).should('be.visible').contains('Examples').click()
-    cy.get(data.APIref).contains('API Preference').click()
   })
 })
